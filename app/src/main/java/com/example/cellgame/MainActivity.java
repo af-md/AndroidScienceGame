9056f3875e7dffe361e6bac2ca4a946f0dcb570a
Afzal Muhammad <afzal.muhammad@manchesteraiports.com>
2021-04-07 11:38:59 +0100

    Background music added

--- app/src/main/java/com/example/cellgame/MainActivity.java
+++ app/src/main/java/com/example/cellgame/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.cellgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer gameBackgroundMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameBackgroundMusic = MediaPlayer.create(this, R.raw.game_sound);
+        gameBackgroundMusic.start();
     }
 }
